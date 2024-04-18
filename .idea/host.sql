1611a116c5cdf7916652f7354fb36912ee4cfebb
Lucas Brennand <100360881+LucasBrennand@users.noreply.github.com>
2024-04-17 22:02:33 -0300

    Create host.sql

---
+++ .idea/host.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE vinho_ecommerce;
+CREATE TABLE products(
+    productID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    description TEXT,
+    price DECIMAL(10, 2),
+    stock_quantity INT
+);
+
+INSERT INTO products(NAME, DESCRIPTION, PRICE, STOCK_QUANTITY)
+VALUES('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5),
+      ('Rose','Vinho tinto',80.00,5);
+
+CREATE TABLE roles (
+    roleID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(5)
+);
+
+INSERT INTO roles (name)
+VALUES('Boss'),
+      ('Manager'),
+      ('Employee'),
+      ('Client');
+
+DROP DATABASE if exists vinho_ecommerce;
